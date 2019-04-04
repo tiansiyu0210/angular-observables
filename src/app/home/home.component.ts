8beab535024bef38d556c77910782b3d3fabab00
tiansiyu0210 <tiansiyu297@gmail.com>
2019-04-03 22:02:22 -0400

    create observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
+import {Observer} from 'rxjs/Observer';
 
 @Component({
   selector: 'app-home',
@@ -12,12 +13,35 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumber = Observable.interval(1000);
-    myNumber.subscribe(
-      (number: number) => {
-        console.log(number);
+    // const myNumber = Observable.interval(1000);
+    // myNumber.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
+
+    const myObs = Observable.create(
+      (obs: Observer<string>) => {
+        setTimeout(
+          () => {
+            obs.next('first package');
+          }, 2000 );
+        setTimeout(
+          () => {
+            obs.next('second package');
+          }, 4000);
+        setTimeout(
+          () => {
+            obs.error('error package');
+          }, 6000);
       }
-    )
+    );
+
+    myObs.subscribe(
+      (data: string) => {console.log(data); },
+      (error: string) => {console.log(error); },
+      () => {console.log('complete'); }
+    );
   }
 
 }
